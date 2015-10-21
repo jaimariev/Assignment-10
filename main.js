f46cd54830ebdf8b290271d64c3817bc5f124250
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-10-20 17:56:21 -0700

    saving

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$("#s1").on('click', function(){
+		$("#content1").toggleClass('displayBlock')
+	});
+
+	$("#s2").on('click', function(){
+		$("#content2").toggleClass('displayBlock')
+	});
+
+	$("#s3").on('click', function(){
+		$("#content3").toggleClass('displayBlock')
+	});
+});
