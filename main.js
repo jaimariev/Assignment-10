ce2c9f873c28174de3c8462e500fdb072b5d26af
Jaimarie Velasco <jaimariev@Jaimaries-MacBook-Pro.local>
2015-10-20 21:58:36 -0700

    commit

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 
 	$("#s1").on('click', function(){
-		$("#content1").toggleClass('displayBlock')
+	$("#content1").toggleClass('displayBlock')
 	});
 
 	$("#s2").on('click', function(){
-		$("#content2").toggleClass('displayBlock')
+	$("#content2").toggleClass('displayBlock')
 	});
 
 	$("#s3").on('click', function(){
-		$("#content3").toggleClass('displayBlock')
+	$("#content3").toggleClass('displayBlock')
 	});
+
 });
